eac7899a65b3e8292d0f01727aac282455009cb8
Odin-byte <dominikwerman@gmail.com>
2024-08-08 16:07:40 +0200

    Added docking to the mode request script

--- ff_examples_ros2/scripts/send_mode_request.py
+++ ff_examples_ros2/scripts/send_mode_request.py
@@ -21,7 +21,7 @@ import rclpy
 from rclpy.node import Node
 
 from rmf_fleet_msgs.msg import RobotMode
-from rmf_fleet_msgs.msg import ModeRequest
+from rmf_fleet_msgs.msg import ModeRequest, ModeParameter
 
 
 def main(argv = sys.argv):
@@ -37,12 +37,14 @@ def main(argv = sys.argv):
     default_robot_name = 'robot_name'
     default_task_id = '576y13ewgyffeijuais'
     default_mode = 'mode'
+    default_dock = 'dock'
     default_topic_name = 'robot_mode_requests'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--fleet-name', default=default_fleet_name)
     parser.add_argument('-r', '--robot-name', default=default_robot_name)
     parser.add_argument('-m', '--mode', default=default_mode)
+    parser.add_argument('-d', '--dock', default=default_dock)
     parser.add_argument('-i', '--task-id', default=default_task_id)
     parser.add_argument('-t', '--topic-name', default=default_topic_name)
     args = parser.parse_args(argv[1:])
@@ -63,14 +65,21 @@ def main(argv = sys.argv):
     msg.task_id = args.task_id
     
     if args.mode == 'mode':
-        print('Please insert desired mode, pause or resume')
+        print('Please insert desired mode, pause, resume or docking')
         return
     elif args.mode == 'pause':
         msg.mode.mode = RobotMode.MODE_PAUSED
     elif args.mode == 'resume':
         msg.mode.mode = RobotMode.MODE_MOVING
+    elif args.mode == 'docking':
+        msg.mode.mode = RobotMode.MODE_DOCKING
+        dock_name_param = ModeParameter()
+        dock_name_param.name = "Dock_Name"
+        dock_name_param.value = args.dock
+        msg.parameters = [dock_name_param]
+        print(msg)
     else:
-        print('unrecognized mode requested, only use pause or resume please')
+        print('unrecognized mode requested, only use pause, resume or docking please')
         return
   
     rclpy.spin_once(node, timeout_sec=1.0)
